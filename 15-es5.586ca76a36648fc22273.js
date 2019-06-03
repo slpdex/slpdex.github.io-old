2f5acc60f08370ddfa6f6597c6292ed27bb9a625
Chipp! <alex@ryberg.com>
2019-06-03 22:09:40 +0200

    build(slpdev): release

--- 15-es5.586ca76a36648fc22273.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{"hGM+":function(i,_,n){"use strict";var w=n.w[i.i];for(var p in n.r(_),w)"__webpack_init__"!=p&&(_[p]=w[p]);n("A6WG"),w.__webpack_init__()}}]);
